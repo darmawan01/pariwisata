c7c1cb404f531bf62947dfb381d21ddc79df8eea
darmawan01 <darmawan.zulkifli@gmail.com>
2018-04-11 00:18:19 +0800

    fix all controller and route

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Country;
 use Illuminate\Http\Request;
 
-class CountryController extends Controller
+class DestinationController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,7 +14,9 @@ class CountryController extends Controller
      */
     public function index()
     {
-        //
+        $country = Country::all();
+
+        return view('post.create', compact('country'));
     }
 
     /**
@@ -24,62 +26,91 @@ class CountryController extends Controller
      */
     public function create()
     {
-        //
+        return view('post.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $this->validate(request(), [
+            'name' => 'required',
+            'continant_id' => 'required',
+        ]);
+
+        Country::create([
+            'name' => request('name'),
+            'continant_id' => request('continant_id'),
+        ]);
+
+        return redirect()->route('post.index')->withSuccess('Data Di Tambahkan !!!');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Country  $country
+     * @param \App\Country $country
+     *
      * @return \Illuminate\Http\Response
      */
     public function show(Country $country)
     {
-        //
+        return view('post.show', compact('country'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Country  $country
+     * @param \App\Country $country
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit(Country $country)
     {
-        //
+        $countrys = Country::all();
+
+        return view('post.edit', compact('country', 'countrys'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Country  $country
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Country             $country
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Country $country)
     {
-        //
+        $this->validate(request(), [
+            'name' => 'required',
+            'continant_id' => 'required',
+        ]);
+
+        Country::create([
+            'name' => request('name'),
+            'continant_id' => request('continant_id'),
+        ]);
+
+        return redirect()->route('post.index')->withInfo('Data Di Ubah !!!');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Country  $country
+     * @param \App\Country $country
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy(Country $country)
     {
-        //
+        $country->delete();
+
+        return redirect()->route('post.index')->withDanger('Data Di Tambahkan !!!');
     }
 }
--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -14,7 +14,9 @@ class DestinationController extends Controller
      */
     public function index()
     {
-        //
+        $destination = Destination::all();
+
+        return view('post.create', compact('destination'));
     }
 
     /**
@@ -24,62 +26,99 @@ class DestinationController extends Controller
      */
     public function create()
     {
-        //
+        return view('post.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $this->validate(request(), [
+            'name' => 'required',
+        ]);
+
+        Destination::create([
+            'name' => request('name'),
+        ]);
+
+        return redirect()->route('post.index')->withSuccess('Data Di Tambahkan !!!');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Destination  $destination
+     * @param \App\Destination $destination
+     *
      * @return \Illuminate\Http\Response
      */
     public function show(Destination $destination)
     {
-        //
+        return view('post.show', compact('destination'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Destination  $destination
+     * @param \App\Destination $destination
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit(Destination $destination)
     {
-        //
+        $destinations = Destination::all();
+
+        return view('post.edit', compact('destination', '$destinations'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Destination  $destination
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Destination         $destination
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Destination $destination)
     {
-        //
+        $this->validate(request(), [
+            'name' => 'required',
+            'gender' => 'required',
+            'age' => 'required',
+            'country_id' => 'required',
+            'nationality_id' => 'required',
+            'destination_id' => 'required',
+            'hotel_id' => 'required',
+        ]);
+
+        Destination::create([
+            'name' => request('name'),
+            'gender' => request('gender'),
+            'age' => request('age'),
+            'country_id' => request('country_id'),
+            'nationality_id' => request('nationality_id'),
+            'destination_id' => request('destination_id'),
+            'hotel_id' => request('hotel_id'),
+        ]);
+
+        return redirect()->route('post.index')->withInfo('Data Di Ubah !!!');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Destination  $destination
+     * @param \App\Destination $destination
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy(Destination $destination)
     {
-        //
+        $destination->delete();
+
+        return redirect()->route('post.index')->withDanger('Data Di Tambahkan !!!');
     }
 }
